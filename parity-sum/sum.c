cc5dd2b9d6e9c9bfc57bfd7f013e4cc786038d1d
Andrew Bloom <blooma116@gmail.com>
2018-02-23 14:42:49 -0800

    refactored sum program

--- parity-sum/sum.c
+++ parity-sum/sum.c
@@ -7,36 +7,34 @@ int main()
     int num;
     int sum1 = 0;
     int sum2 = 0;
-
-    printf("Enter your first number: ");
-    fflush(stdin);
-    scanf("%d", &num);
     
-    if (num % 2 == 0)
-        sum1 += num;
-    else
-        sum2 += num;
-
-    printf("Enter your second number: ");
-    fflush(stdin);
-    scanf("%d", &num);
+    printf("This program will return a sum of the even and odd numbers you enter.\n");
+    printf("Enter 0 to exit this sequence.\n");
     
-    if (num % 2 == 0)
-        sum1 += num;
-    else
-        sum2 += num;
-
-    printf("Enter your third number: ");
-    fflush(stdin);
-    scanf("%d", &num);
+    do
+    {
+        printf("\nEnter a number: ");
+        fflush(stdin);
+        scanf("%d", &num);
+        
+        if (num == 0) break;
+        
+        switch (num % 2)
+        {
+            case 0:
+                sum1 += num;
+                break;
+            case 1:
+                sum2 += num;
+                break;
+        }
+        
+        printf("\nThe running total of your even numbers is %d\n", sum1);
+        printf("The running total of your odd numbers is %d\n", sum2);
+    } while (num);
     
-    if (num % 2 == 0)
-        sum1 += num;
-    else
-        sum2 += num;
-    
-    printf("\nThe sum of your even numbers is %d\n", sum1);
-    printf("The sum of your odd numbers is %d\n", sum2);
+    printf("\nThe sum of your even numbers comes to %d\n", sum1);
+    printf("The sum of your odd numbers comes to %d\n", sum2);
 
     return 0;
 }
