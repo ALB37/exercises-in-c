11967c57241863812e9367414dcc8203ecb855b6
Andrew Bloom <blooma116@gmail.com>
2018-02-23 16:39:04 -0800

    wrote simple program to get info about an arbitrary number of employees and display that information

---
+++ employee-list/employees.c
@@ -0,0 +1,65 @@
+/* employees.c: request information about an arbitrary number of employees*/
+
+#include <stdio.h>
+#include <string.h>
+
+struct Employee {
+	char last[16];
+	char first[11];
+	char position[11];
+	int salary;
+};
+
+struct Company {
+	struct Employee staff[BUFSIZ];
+	int employeeCount;
+};
+
+int main() {
+	struct Company co;
+	int i, wage;
+	char lname[16];
+	char fname[11];
+	char title[11];
+	co.employeeCount = 0;
+
+	printf("Enter info about your company's employees below\n");
+	printf("To exit data entry, enter a 0 as an employee's last name\n");
+
+	while (1) {
+		printf("\nEnter an employee's last name: ");
+		fflush(stdin);
+		scanf("%s", lname);
+
+		if (lname[0] == '0') break;
+
+		printf("Enter that employee's first name: ");
+		fflush(stdin);
+		scanf("%s", fname);
+
+		printf("Enter that employee's title: ");
+		fflush(stdin);
+		scanf("%s", title);
+
+		printf("Enter that employee's salary: ");
+		fflush(stdin);
+		scanf("%d", &wage);
+
+		strcpy(co.staff[co.employeeCount].last, lname);
+		strcpy(co.staff[co.employeeCount].first, fname);
+		strcpy(co.staff[co.employeeCount].position, title);
+		co.staff[co.employeeCount].salary = wage;
+		co.employeeCount++;
+	}
+
+	printf("\nEmployee List:\n\n");
+
+	for (i = 0; i < co.employeeCount; i++)
+		printf("%s: %s, %s; salary: $%d\n",
+			   co.staff[i].position,
+			   co.staff[i].last,
+			   co.staff[i].first,
+			   co.staff[i].salary);
+	
+	return 0;
+}
